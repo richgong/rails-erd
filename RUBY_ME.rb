7dd9a0656f6645c2db9a86f887ed21ab4d72a36c
Richard Gong <richard.gong@instacart.com>
2019-04-13 00:34:22 -0700

    hackedy hack

--- RUBY_ME.rb
+++ RUBY_ME.rb
@@ -3,12 +3,28 @@ $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 lib = File.expand_path "~/.rbenv/versions/2.5.5/lib/ruby/gems/2.5.0/gems/ruby-graphviz-1.2.4/lib"
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
-require "~/c/ghost/config/environment.rb"
+puts "Loading app..."
+ENV['DISABLE_SPRING']='1'
+path = '~/carrot/customers/instacart'
+# path = '~/c/ghost'
+# path = '~/c/tux'
 
+old_path = Dir.pwd
+path = File.expand_path path
+Dir.chdir path
+env_path = File.join path, "config/environment.rb"
+require env_path
 Rails.application.eager_load!
 Rails.application.config.eager_load_namespaces.each(&:eager_load!) if Rails.application.config.respond_to?(:eager_load_namespaces)
+
+
 puts "Generating Entity-Relationship Diagram for #{ActiveRecord::Base.descendants.length} models..."
+Dir.chdir old_path
+# load_path.each { |path| $LOAD_PATH.unshift(path) unless $LOAD_PATH.include?(path) }
+# require 'ruby-graphviz'
 
 require 'ruby-graphviz'
 require "rails_erd/diagram/graphviz"
 RailsERD::Diagram::Graphviz.create
+
+puts "Done generating diagram!"
