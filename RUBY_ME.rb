0f99cb260f23c6e319615b3204b3e5bce61b0582
Richard Gong <richard.gong@instacart.com>
2019-04-13 01:25:07 -0700

    verified working

--- RUBY_ME.rb
+++ RUBY_ME.rb
@@ -3,12 +3,12 @@ require 'ruby-graphviz'
 load_path = []
 $LOAD_PATH.each { |path| load_path << path }
 
-#lib = File.expand_path "~/.rbenv/versions/2.5.5/lib/ruby/gems/2.5.0/gems/ruby-graphviz-1.2.4/lib"
-#$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+# lib = File.expand_path "~/.rbenv/versions/2.5.5/lib/ruby/gems/2.5.0/gems/ruby-graphviz-1.2.4/lib"
+# $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
 puts "Loading app..."
-path = '~/carrot/customers/instacart'
-# path = '~/c/ghost'
+# path = '~/carrot/customers/instacart'
+path = '~/c/ghost'
 # path = '~/c/tux'
 
 old_path = Dir.pwd
@@ -26,6 +26,7 @@ Dir.chdir old_path
 load_path.each { |path| $LOAD_PATH.unshift(path) unless $LOAD_PATH.include?(path) }
 lib = File.expand_path("../lib", __FILE__) # doesn't matter if run via `ruby` or `irb`
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+
 
 require "rails_erd/diagram/graphviz"
 RailsERD::Diagram::Graphviz.create
